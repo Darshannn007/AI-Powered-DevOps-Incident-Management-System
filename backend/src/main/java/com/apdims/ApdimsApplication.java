f7af8dbec25b74271568ede9d511d6e049aab7be
DARSHAN <darshandesale143@gmail.com>
2026-09-04 09:31:24 +0530

    First commit

---
+++ backend/src/main/java/com/apdims/ApdimsApplication.java
@@ -0,0 +1,13 @@
+package com.apdims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApdimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApdimsApplication.class, args);
+	}
+
+}
